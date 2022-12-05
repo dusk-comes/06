4e8a5a1aaa4556aeb056c48674fab21dcc249dc9
ak <14f143@gmail.com>
2022-12-06 02:01:29 +0300

    added the main program

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "matrix.hpp"
 #include "cassert"
+#include <iostream>
 
 int main()
 {
@@ -7,6 +8,36 @@ int main()
     m[2][2] = 3;
     [[maybe_unused]] int val = m[2][2];
     assert(m[1][1] == 0);
+
+    Matrix<int, 0> matrix;
+
+    for (size_t i = 0, j = 0; i < 10; ++i, ++j)
+    {
+        matrix[i][j] = i;
+    }
+
+    for (size_t i = 0, j = 9; i < 10; ++i, --j)
+    {
+        matrix[i][j] = j;
+    }
+
+    for (size_t i = 1; i < 9; ++i)
+    {
+        for(size_t j = 1; j < 9; ++j)
+        {
+            std::cout << matrix[i][j] << " ";
+        }
+
+        std::cout << std::endl;
+    }
+
+    std::cout << matrix.size() << std::endl;
+
+    for (const auto &[axes, value] : matrix)
+    {
+        const auto [x, y] = axes;
+        std::cout << "[" << x << "," << y << "]" << " " << value << std::endl;
+    }
 
     return 0;
 }
